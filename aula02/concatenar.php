734c0d53766071b6b92c0cc1e08ef5ea0c5fa624
Gustavo Lopez <gugianelli@gmail.com>
2025-02-26 10:29:17 -0300

    Aula 2 de PHP

---
+++ aula02/concatenar.php
@@ -0,0 +1,14 @@
+<?php 
+    ECHO "Hello World!<br>";
+    echo "Hello World!<br>";
+    EcHo "Hello World!<br>";
+
+    $nome = "Pedro";
+    $sobrenome = "Brito";
+    $numero = 16;
+
+    echo "${nome} <h1> ${sobrenome} </h1>";
+    echo $numero . "<p> " . $nome . " " . $sobrenome . "</p>";
+?>
+
+<link href="../aula02/concatenar.css" type="text/css"  rel="stylesheet">
---
+++ aula03/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <body>
+    <style>
+        .red{
+            color:rgb(255, 0, 0);
+            padding: 10px;
+            font-size: 80px;
+        }
+        .gust{
+            color:rgb(0, 17, 255);
+            font-weight: 100;
+        }
+        .pedr{
+            color:rgb(153, 0, 255);
+            font-weight: 100;
+        }
+    </style>   
+    
+        <p class="red"> Dupla dinâmica: <b class="gust">Gustavo Lopez</b> e <b class="pedr">Pedro Henrique</b> </p>
+
+    </body>
+</html>
+
