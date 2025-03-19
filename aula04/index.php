16ce505ff02cd7df9a5beafe350c4fc4d83aec85
Gustavo Lopez <gugianelli@gmail.com>
2025-03-19 10:28:52 -0300

    Aula 4 sobre PHP

---
+++ aula04/index.php
@@ -0,0 +1,24 @@
+<?php  
+$i = 1;
+
+while ($i < 6) {
+  echo $i;
+  $i++;
+} 
+?>
+
+<?php  
+for ($x = 0; $x <= 10; $x++) {
+  echo "The number is: $x <br>";
+}
+?>  
+
+<?php  
+$i = 1;
+
+
+do {
+  echo $i;
+  $i++;
+} while ($i < 6);
+?>  
