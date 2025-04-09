5209a128ed9462a3d8950d5f71fa5290072dff91
Gustavo Lopez <gugianelli@gmail.com>
2025-04-09 10:32:08 -0300

    Aula 05

---
+++ aula05/funcao.php
@@ -0,0 +1,22 @@
+<?php
+    function myMessage() {
+        echo "Hello World! <br>";
+    }
+
+    myMessage();
+    myMessage();
+
+    function soma($n1, $n2) {
+        echo $n1 + $n2, "<br>";
+    }
+
+    soma(3, 4);
+    soma(3, 10);
+
+    function cadastrar() {
+        // Função com lógica para cadastrar
+    }
+    function atualizarDados() {
+        // Função com lógica para atualizar dados
+    }
+?>
